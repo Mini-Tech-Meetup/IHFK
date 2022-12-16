859ff3db22b40a1001b13507b310ef8dc9d3d832
JHyunB <whdgus4892@gmail.com>
2022-12-17 00:34:40 +0900

    feat: 시작화면 문구 출력

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -11,28 +11,42 @@ function randomPosition() {
 	return [randomX, randomY]
 }
 
+
+
+
 export default class StartScene extends Phaser.Scene {
+	
 	constructor() {
-		super('start')
+		super({key: 'StartScene'});
 	}
 
 	init() {
-
+		this.time.addEvent({
+			delay: 1000, // 1초마다 실행
+			callback: this.updateImage,
+			callbackScope: this,
+			loop: true
+		});
 	}
 
 	preload() {
-		this.load.image('where_bubble', 'assets/speech_bubble/where.png')
-
+		this.load.image('where_bubble', 'assets/speech_bubble/where.png');
 	}
 
 	create() {
-		console.log('StartScene.create()')
-		var [x, y] = randomPosition()
-		var bubble = this.add.image(x, y, 'where_bubble').setOrigin(0)
-
+		console.log('StartScene.create()');
+		var [x, y] = randomPosition();
+		this.bubble = this.add.image(x, y, 'where_bubble').setOrigin(0.5,0.5);
 	}
 
 	update(time, delta) {
+		
+	}
+
+	updateImage() {
+		var [x, y] = randomPosition();
+		this.bubble.destroy();
+		this.bubble = this.add.image(x, y, 'where_bubble').setOrigin(0.5,0.5);
 	}
 	
 }
